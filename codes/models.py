e338166dc35f73c51efcbc43f26bf4573f37f34f
Tomas Mc <tommcn@Ds-Mac-mini.local>
2020-04-09 08:52:15 -0400

    vlass/groupe

--- codes/models.py
+++ codes/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class classe(models.Model):
     nom = models.CharField(max_length=127)
-    classe_groupe = models.CharField(max_length=2)
+    classe_groupe = models.CharField(max_length=8)
     prof = models.CharField(max_length=31)
     commnence = models.DateTimeField()
     fini = models.DateTimeField()
