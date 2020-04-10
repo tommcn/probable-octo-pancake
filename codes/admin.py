ca408dff56e8dc1ea15ec15f748139a0ddf476ad
Tomas Mc <tommcn@Ds-Mac-mini.local>
2020-04-10 15:26:27 -0400

    Actions

--- codes/admin.py
+++ codes/admin.py
@@ -2,10 +2,22 @@ from django.contrib import admin
 from .models import classe
 
 # Register your models here.
+
+def publish(ModelAdmin, request, queryset):
+    queryset.update(posted=True)
+publish.short_description = "Post selected classes"
+
+def unpublish(ModelAdmin, request, queryset):
+    queryset.update(posted=False)
+unpublish.short_description = "Remove selected classes from site"
+
+
+
 @admin.register(classe)
 class classeA(admin.ModelAdmin):
     search_fields = ['nom', 'classe_groupe', 'prof', 'commnence', 'fini']
     list_display = ('nom', 'classe_groupe', 'prof', 'commnence', 'code', 'posted')
     list_filter = ('classe_groupe','prof', 'posted')
+    actions = [publish, unpublish]
 
 admin.site.site_header = "Interface d'Administration"
